8a2ef48905587f6bb928751099588bce8a8a5766
israel chavez <izrra.ch@icloud.com>
2020-03-03 22:23:50 -0700

    +procesador de comandos #perfil de publicacion

---
+++ Watari/Funciones/Sql/Rutinas.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Watari.Enumerados;
+using Watari.Modelos;
+
+namespace Watari.Funciones.Sql
+{
+  /// <summary>
+  /// Provee el mecanismo para manipular las rutinas asociadas
+  /// al esquema del repositorio de datos
+  /// </summary>
+  public sealed class FuncionRutinasSql
+  {
+    #region Propiedades
+
+    /// <summary>
+    /// Configuracion de conexion hacia el repositorio de datos
+    /// </summary>
+    private string CadenaDeConexion { get; }
+
+    /// <summary>
+    /// Directorio para almacenar las rutinas
+    /// </summary>
+    private string DirectorioEsquema { get; }
+
+    /// <summary>
+    /// Directorio para almacenar los procedimientos
+    /// </summary>
+    private string DirectorioProcedimientos => DirectorioEsquema + @"Procedimientos\";
+
+    /// <summary>
+    /// Directorio para almacenar las funciones escalares
+    /// </summary>
+    private string DirectorioFuncionesEscalares => DirectorioEsquema + @"Funciones\Escalar\";
+
+    /// <summary>
+    /// Directorio para almacenar las funciones de tabla
+    /// </summary>
+    private string DirectorioFuncionesTabla => DirectorioEsquema + @"Funciones\Tabla\";
+
+    /// <summary>
+    /// Instruccion sql para obtener todas las rutina que no son de sistema
+    /// </summary>
+    private const string SqlObtenerRutinas = @"
+      SELECT
+          SPECIFIC_SCHEMA AS Esquema,
+          SPECIFIC_NAME AS Nombre,
+          (CASE WHEN ROUTINE_TYPE = N'PROCEDURE'
+                THEN 0
+                WHEN ROUTINE_TYPE = N'FUNCTION' AND DATA_TYPE <> N'TABLE'
+                THEN 1
+                WHEN ROUTINE_TYPE = N'FUNCTION' AND DATA_TYPE = N'TABLE'
+                THEN 2
+          END) AS Tipo,
+          ROUTINE_DEFINITION AS Definicion,
+          DATA_TYPE AS SubTipo,
+          CREATED AS Creado,
+	        LAST_ALTERED AS Modificado
+      FROM INFORMATION_SCHEMA.ROUTINES
+      WHERE LEFT(ROUTINE_NAME, 3) NOT IN ('sp_', 'xp_', 'ms_')";
+
+    #endregion
+
+    public FuncionRutinasSql(string cadenaDeConexion, string directorioEsquema)
+    {
+      CadenaDeConexion = cadenaDeConexion;
+      DirectorioEsquema = directorioEsquema.EndsWith(@"\")
+        ? directorioEsquema
+        : directorioEsquema + @"\";
+    }
+
+    #region Metodos Publicos
+
+    /// <summary>
+    /// Agrega/Actualiza/Elimina todas las rutinas sql
+    /// asociadas al esquema del repositorio de datos
+    /// </summary>
+    /// <returns></returns>
+    public async Task Sincronizar()
+    {
+      if (CadenaDeConexion.Trim().Length.Equals(0))
+      {
+        Console.WriteLine(@"La cadena de conexion en el archivo de configuracion no es valida.");
+        return;
+      }
+      if (DirectorioEsquema.Trim().Length.Equals(0))
+      {
+        Console.WriteLine(@"El directorio de almacenamiento en el archivo de configuracion no es valido.");
+        return;
+      }
+      using (SqlConnection conexion = new SqlConnection(CadenaDeConexion))
+      {
+        try
+        {
+          //Establecer la conexion con el repositorio
+          await conexion.OpenAsync();
+          List<RutinaSql> rutinas = null;
+          using (SqlCommand comando = new SqlCommand(SqlObtenerRutinas, conexion))
+          {
+            using (SqlDataReader lector = await comando.ExecuteReaderAsync())
+            {
+              if (lector.HasRows)
+              {
+                //Leer todas las rutinas
+                rutinas = new List<RutinaSql>();
+                while (await lector.ReadAsync())
+                {
+                  RutinaSql rutina = new RutinaSql()
+                  {
+                    Nombre = $@"[{lector.GetString(0)}].[{lector.GetString(1)}]",
+                    Tipo = (TipoRutina)lector.GetInt32(2),
+                    Definicion = lector.GetString(3)
+                  };
+                  rutinas.Add(rutina);
+                }
+                Console.WriteLine($@"Funciones Escalar : {rutinas.Count(r => r.Tipo.Equals(TipoRutina.FuncionEscalar))}.");
+                Console.WriteLine($@"Funciones Tabla : {rutinas.Count(r => r.Tipo.Equals(TipoRutina.FuncionTabla))}.");
+                Console.WriteLine($@"Procedimientos Almacenados : {rutinas.Count(r => r.Tipo.Equals(TipoRutina.Procedimiento))}.");
+                //Eliminar todas las rutinas anteriores
+                EliminarRutinasAnteriores();
+              }
+              else
+              {
+                Console.WriteLine(@"No hay ninguna rutina asociada al esquema.");
+              }
+              lector.Dispose();
+            }
+            comando.Dispose();
+          }
+          //Terminar la sesion
+          if (conexion.State.Equals(ConnectionState.Open))
+            conexion.Close();
+          //Guardar todas las rutinas
+          if (rutinas != null && rutinas.Any())
+            await GuardarRutinasEnArchivos(rutinas);
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e);
+        }
+        conexion.Dispose();
+      }
+    }
+
+    #endregion
+
+    #region Metodos Privados
+
+    /// <summary>
+    /// Determina si existen los directorios asociados a las rutinas
+    /// sql y si no, los crea.
+    /// </summary>
+    private void InicializarDirectoriosDelEsquema()
+    {
+      //Comprobar que exista el directorio de procedimientos, si no, crearlo.
+      if (!Directory.Exists(DirectorioProcedimientos))
+        Directory.CreateDirectory(DirectorioProcedimientos);
+
+      //Comprobar que exista el directorio de funciones, si no, crearlo.
+      if (!Directory.Exists(DirectorioEsquema + @"Funciones"))
+        Directory.CreateDirectory(DirectorioEsquema + @"Funciones");
+
+      //Comprobar que exista el directorio de funciones escalares, si no, crearlo.
+      if (!Directory.Exists(DirectorioFuncionesEscalares))
+        Directory.CreateDirectory(DirectorioFuncionesEscalares);
+
+      //Comprobar que exista el directorio de funciones tipo tabla, si no, crearlo.
+      if (!Directory.Exists(DirectorioFuncionesTabla))
+        Directory.CreateDirectory(DirectorioFuncionesTabla);
+    }
+
+    /// <summary>
+    /// Elimina todos los archivos de rutina anteriores
+    /// y prepara los directorios para archivas las nuevas
+    /// </summary>
+    private void EliminarRutinasAnteriores()
+    {
+      try
+      {
+        Directory.Delete(DirectorioProcedimientos, true);
+        Directory.Delete(DirectorioFuncionesTabla, true);
+        Directory.Delete(DirectorioFuncionesEscalares, true);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($@"No se han podido eliminar las rutinas anteriores {e.Message}");
+      }
+      InicializarDirectoriosDelEsquema();
+    }
+
+    /// <summary>
+    /// Guarda un listado de rutinas como archivos
+    /// </summary>
+    /// <param name="rutinas">Rutinas sql</param>
+    /// <returns></returns>
+    private async Task GuardarRutinasEnArchivos(List<RutinaSql> rutinas)
+    {
+      //Si no hay rutinas definidas se termina la tarea
+      if (rutinas == null || !rutinas.Any()) return;
+
+      for (int x = 0; x < rutinas.Count; x++)
+      {
+        RutinaSql r = rutinas.ElementAt(x);
+        string directorio;
+        //Determinar el tipo de rutina
+        switch (r.Tipo)
+        {
+          case TipoRutina.Procedimiento:
+            directorio = DirectorioProcedimientos;
+            break;
+          case TipoRutina.FuncionEscalar:
+            directorio = DirectorioFuncionesEscalares;
+            break;
+          case TipoRutina.FuncionTabla:
+            directorio = DirectorioFuncionesTabla;
+            break;
+          default:
+            directorio = DirectorioEsquema;
+            break;
+        }
+        string urlArchivo = $@"{directorio}{r.Nombre}.sql";
+        //Abrir o escribir el archivo
+        using (FileStream fs = File.OpenWrite(urlArchivo))
+        {
+          fs.Position = 0;
+          Console.WriteLine($@"Escribiendo archivo {r.Nombre}.");
+          //Escribir el contenido del archivo desde la posicion 0
+          using (StreamWriter sw = new StreamWriter(fs))
+          {
+            sw.AutoFlush = true;
+            await sw.WriteAsync(r.Definicion);
+            sw.Dispose();
+          }
+          fs.Dispose();
+        }
+      }
+    }
+
+    #endregion
+  }
+}
--- Watari/Program.cs
+++ Watari/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Watari.Enumerados;
+using Watari.Funciones.Sql;
 using Watari.Modelos;
 
 namespace Watari
@@ -21,30 +17,14 @@ namespace Watari
     #region Propiedades
 
     /// <summary>
-    /// Instruccion sql para obtener todas las rutina que no son de sistema
-    /// </summary>
-    private const string SqlObtenerRutinas = @"
-      SELECT
-          SPECIFIC_SCHEMA AS Esquema,
-          SPECIFIC_NAME AS Nombre,
-          (CASE WHEN ROUTINE_TYPE = N'PROCEDURE'
-                THEN 0
-                WHEN ROUTINE_TYPE = N'FUNCTION' AND DATA_TYPE <> N'TABLE'
-                THEN 1
-                WHEN ROUTINE_TYPE = N'FUNCTION' AND DATA_TYPE = N'TABLE'
-                THEN 2
-          END) AS Tipo,
-          ROUTINE_DEFINITION AS Definicion,
-          DATA_TYPE AS SubTipo,
-          CREATED AS Creado,
-	        LAST_ALTERED AS Modificado
-      FROM INFORMATION_SCHEMA.ROUTINES
-      WHERE LEFT(ROUTINE_NAME, 3) NOT IN ('sp_', 'xp_', 'ms_')";
-
-    /// <summary>
     /// Configuracion asociada al contexto
     /// </summary>
     private static ConfiguracionContexto Configuracion { get; set; }
+
+    /// <summary>
+    /// Funciones de sql sobre rutinas
+    /// </summary>
+    private static FuncionRutinasSql FuncionRutinasSql { get; set; }
 
     #endregion
 
@@ -54,23 +34,30 @@ namespace Watari
     /// <param name="args">Argumentos</param>
     static void Main(string[] args)
     {
-      string rutaConfiguracion = @"";
-      //Si se ha suplido un argumento se interpreta como ruta hacia la configuracion
-      if (!args.Length.Equals(0))
-        rutaConfiguracion = args[0];
-      if (rutaConfiguracion == null || rutaConfiguracion.Trim().Length.Equals(0))
+      //Verificar los argumentos
+      if (args == null || args.Length.Equals(0))
       {
-        Console.WriteLine(@"No has proporcionado un archivo de configuracion, buscando uno...");
-        DirectoryInfo info = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
-        string directorioSuperior = info.Parent?.ToString() ?? @"";
-        directorioSuperior = directorioSuperior.EndsWith(@"\") ? directorioSuperior : directorioSuperior + @"\";
-        string directorioConfiguracion = directorioSuperior + @"ConfiguracionContexto.json";
-        if (File.Exists(directorioConfiguracion))
-        {
-          rutaConfiguracion = directorioConfiguracion;
-          Console.WriteLine($@"Utilizando el archivo de configuracion: '{directorioConfiguracion}'");
-        }
+        MostrarAyuda();
+        return;
       }
+      //Inicializar el entorno de trabajo
+      Inicializar();
+      //Procesar el comando solicitado
+      ProcesarComando(args).Wait();
+      Console.WriteLine(@":p");
+    }
+
+    #region Metodos Privados
+
+    /// <summary>
+    /// Establece el ambito de trabajo
+    /// </summary>
+    private static void Inicializar()
+    {
+      #region Inicializar la configuracion
+
+      //Ruta hacia el archivo de configuracion
+      string rutaConfiguracion = AppDomain.CurrentDomain.BaseDirectory + @"Configuracion.json";
       //Si no existe el archivo de configuracion se termina la tarea
       if (!File.Exists(rutaConfiguracion))
       {
@@ -78,6 +65,7 @@ namespace Watari
         return;
       }
       StringBuilder sb = new StringBuilder();
+      //Leer archivo
       using (FileStream fs = File.OpenRead(rutaConfiguracion))
       {
         //Leer la configuracion
@@ -96,159 +84,70 @@ namespace Watari
         //Establecer valores de configuracion
         Configuracion = JsonConvert.DeserializeObject<ConfiguracionContexto>(sb.ToString());
         sb.Clear();
-        SincronizarEsquema().Wait();
       }
       catch (Exception)
       {
         Console.WriteLine(@"No se ha podido interpretar el archivo de configuracion.");
       }
-      Console.WriteLine(@"Ha concluido el proceso de sincronizacion.");
-    }
-
-    #region Metodos Privados
-
-    /// <summary>
-    /// Obtiene todas las rutinas sql y actualiza
-    /// las asociadas conforme a los directorios proporcionados
-    /// </summary>
-    /// <returns></returns>
-    private static async Task SincronizarEsquema()
-    {
-      if (Configuracion.CadenaDeConexion.Trim().Length.Equals(0))
-      {
-        Console.WriteLine(@"La cadena de conexion en el archivo de configuracion no es valida.");
-        return;
-      }
-      if (Configuracion.DirectorioEsquema.Trim().Length.Equals(0))
-      {
-        Console.WriteLine(@"El directorio de almacenamiento en el archivo de configuracion no es valido.");
-        return;
-      }
-      using (SqlConnection conexion = new SqlConnection(Configuracion.CadenaDeConexion))
-      {
-        try
-        {
-          //Establecer la conexion con el repositorio
-          await conexion.OpenAsync();
-          List<RutinaSql> rutinas = null;
-          using (SqlCommand comando = new SqlCommand(SqlObtenerRutinas, conexion))
-          {
-            using (SqlDataReader lector = await comando.ExecuteReaderAsync())
-            {
-              if (lector.HasRows)
-              {
-                //Leer todas las rutinas
-                rutinas = new List<RutinaSql>();
-                while (await lector.ReadAsync())
-                {
-                  RutinaSql rutina = new RutinaSql()
-                  {
-                    Nombre = $@"[{lector.GetString(0)}].[{lector.GetString(1)}]",
-                    Tipo = (TipoRutina)lector.GetInt32(2),
-                    Definicion = lector.GetString(3)
-                  };
-                  rutinas.Add(rutina);
-                }
-              }
-              lector.Dispose();
-            }
-            comando.Dispose();
-          }
-          //Terminar la sesion
-          if (conexion.State.Equals(ConnectionState.Open))
-            conexion.Close();
-          //Guardar todas las rutinas
-          if (rutinas != null && rutinas.Any())
-            await GuardarArchivoRutinas(rutinas);
-        }
-        catch (Exception e)
-        {
-          Console.WriteLine(e);
-        }
-        conexion.Dispose();
-      }
-    }
-
-    /// <summary>
-    /// Guarda un listado de rutinas como archivos
-    /// </summary>
-    /// <param name="rutinas">Rutinas sql</param>
-    /// <returns></returns>
-    private static async Task GuardarArchivoRutinas(List<RutinaSql> rutinas)
-    {
-      //Si no hay rutinas definidas se termina la tarea
-      if (rutinas == null || !rutinas.Any()) return;
-
-      #region Verificacion de directorios
-
-      Configuracion.DirectorioEsquema = Configuracion.DirectorioEsquema.EndsWith(@"\")
-        ? Configuracion.DirectorioEsquema
-        : Configuracion.DirectorioEsquema + @"\";
-
-      //Directorio para almacenar los procedimientos
-      string directorioProcedimientos = Configuracion.DirectorioEsquema + @"Procedimientos\";
-
-      //Directorio para almacenar las funciones escalares
-      string directorioFuncionesEscalares = Configuracion.DirectorioEsquema + @"Funciones\Escalar\";
-
-      //Directorio para almacenar las funciones de tabla
-      string directorioFuncionesTabla = Configuracion.DirectorioEsquema + @"Funciones\Tabla\";
-
-      //Comprobar que exista el directorio de procedimientos, si no, crearlo.
-      if (!Directory.Exists(directorioProcedimientos))
-        Directory.CreateDirectory(directorioProcedimientos);
-
-      //Comprobar que exista el directorio de funciones, si no, crearlo.
-      if (!Directory.Exists(Configuracion.DirectorioEsquema + @"Funciones"))
-        Directory.CreateDirectory(Configuracion.DirectorioEsquema + @"Funciones");
-
-      //Comprobar que exista el directorio de funciones escalares, si no, crearlo.
-      if (!Directory.Exists(directorioFuncionesEscalares))
-        Directory.CreateDirectory(directorioFuncionesEscalares);
-
-      //Comprobar que exista el directorio de funciones tipo tabla, si no, crearlo.
-      if (!Directory.Exists(directorioFuncionesTabla))
-        Directory.CreateDirectory(directorioFuncionesTabla);
-
 
       #endregion
 
-      for (int x = 0; x < rutinas.Count; x++)
+      #region Inicializar las instancias
+
+      FuncionRutinasSql = new FuncionRutinasSql(Configuracion.CadenaDeConexion, Configuracion.DirectorioEsquema);
+
+      #endregion
+    }
+
+    /// <summary>
+    /// Determina la funcion y operacion a relizar
+    /// segun el comando proporcionado e invoca su tarea
+    /// </summary>
+    /// <param name="comando">Secuencia de instrucciones</param>
+    /// <returns></returns>
+    private static async Task ProcesarComando(string[] args)
+    {
+      //El primer valor de argumento debe ser la funcion
+      string funcion = args[0].ToLowerInvariant();
+      //El primer valor de argumento debe ser la operacion de la funcion a ejecutar
+      string operacion = args[1].ToLowerInvariant();
+      //El tercer valor de argumento debe ser el parametro para la operacion
+      string parametro = args[2].ToLowerInvariant();
+      switch (funcion)
       {
-        RutinaSql r = rutinas.ElementAt(x);
-        string directorio;
-        //Determinar el tipo de rutina
-        switch (r.Tipo)
-        {
-          case TipoRutina.Procedimiento:
-            directorio = directorioProcedimientos;
-            break;
-          case TipoRutina.FuncionEscalar:
-            directorio = directorioFuncionesEscalares;
-            break;
-          case TipoRutina.FuncionTabla:
-            directorio = directorioFuncionesTabla;
-            break;
-          default:
-            directorio = Configuracion.DirectorioEsquema;
-            break;
-        }
-        string urlArchivo = $@"{directorio}{r.Nombre}.sql";
-        //Abrir o escribir el archivo
-        using (FileStream fs = File.OpenWrite(urlArchivo))
-        {
-          fs.Position = 0;
-          Console.WriteLine($@"Escribiendo archivo {r.Nombre}.");
-          //Escribir el contenido del archivo desde la posicion 0
-          using (StreamWriter sw = new StreamWriter(fs))
+        case @"sql":
+          switch (operacion)
           {
-            sw.AutoFlush = true;
-            await sw.WriteAsync(r.Definicion);
-            sw.Dispose();
+            case @"-rutinassql":
+              switch (parametro)
+              {
+                case @"-sincronizar":
+                  await FuncionRutinasSql.Sincronizar();
+                  break;
+                default:
+                  MostrarAyuda();
+                  break;
+              }
+              break;
+            default:
+              MostrarAyuda();
+              break;
           }
-          fs.Dispose();
-        }
+          break;
+        default:
+          MostrarAyuda();
+          break;
       }
+    }
+
+    /// <summary>
+    /// Muestra la ayuda de la aplicacion
+    /// </summary>
+    private static void MostrarAyuda()
+    {
+      Console.WriteLine(@"-------------Opciones---------------");
+      Console.WriteLine(@">Sql -Rutinas -Sincronizar");
+      Console.WriteLine(@"------------------------------------");
     }
 
     #endregion
